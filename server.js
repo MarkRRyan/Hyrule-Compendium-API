e60c3258279ebed2e9ee8e7dcfa92e672ab68a67
Mark Ryan <markrryan99@gmail.com>
2023-07-19 12:24:50 -0700

    setting up to work with mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const monsters = require('./models/monsters')
 
 const app = express();
+
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+// Connection to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
+
+// Controllers
 
 app.get('/', (req, res) => {
     res.send('Welcome to Hyrule')
@@ -39,6 +61,7 @@ app.get('/monsters/:param', (req, res) => {
     res.send(monster)
 })
 
-app.listen(3000, () => {
-    console.log(`Fighting monsters on 3000`);
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Fighting monsters on ${PORT}`);
 });
