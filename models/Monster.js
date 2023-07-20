c1f6e7ad9fe348e9381ce25ab6e45b131357b6f4
Mark Ryan <markrryan99@gmail.com>
2023-07-20 13:56:10 -0700

    some reorigizing and getting ready for future seed files

--- models/Monster.js
+++ models/Monster.js
@@ -27,6 +27,10 @@ const monsterSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+      image: {
+        type: String,
+        required: true,
+      },
 })
 
 const Monster = mongoose.model('Monster', monsterSchema)
--- seeds/bokoblinSeed.js
+++ seeds/bokoblinSeed.js
@@ -1,9 +1,7 @@
-// seed.js
 const mongoose = require('mongoose');
-const Monster = require('./models/Monster');
+const Monster = require('../models/Monster');
 require('dotenv').config()
 
-// Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -28,7 +26,8 @@ const monstersData = [
             'Bokoblin Horn',
             'Bokobin Fang'
         ],
-        description: `This common species is a nuisance all over Hyrule. Some have unified in the time following the Great Calamity and have formed factions of bandits. While not very clever, they are at least intelligent enough to hunt beasts and grill the meat for food. Though they're typically ferocious carnivores, they actually enjoy fruit as well`
+        description: `This common species is a nuisance all over Hyrule. Some have unified in the time following the Great Calamity and have formed factions of bandits. While not very clever, they are at least intelligent enough to hunt beasts and grill the meat for food. Though they're typically ferocious carnivores, they actually enjoy fruit as well`,
+        image: `https://i.imgur.com/uRWW34v.jpg`
     },
     {
         compendiumId: 104,
@@ -43,7 +42,8 @@ const monstersData = [
             'Bokobin Fang',
             'Bokoblin Guts'
         ],
-        description: `This common species is a nuisance all over Hyrule. They're tougher and have stronger weapons than the red Bokoblins-and are a little more clever, as well. At the very least, they figured out that they can simply kick a remote bomb out of the way to avoid it's blast`
+        description: `This common species is a nuisance all over Hyrule. They're tougher and have stronger weapons than the red Bokoblins-and are a little more clever, as well. At the very least, they figured out that they can simply kick a remote bomb out of the way to avoid it's blast`,
+        image: `https://i.imgur.com/tFKITMk.png`
     },
     {
         compendiumId: 105,
@@ -58,7 +58,8 @@ const monstersData = [
             'Bokobin Fang',
             'Bokoblin Guts'
         ],
-        description: `...`
+        description: `...`,
+        image: `https://i.imgur.com/FfufpDq.jpg`
     },
     {
         compendiumId: 106,
@@ -73,7 +74,8 @@ const monstersData = [
             'Bokobin Fang',
             'Bokoblin Arm'
         ],
-        description: `The remains of Bokoblins appear in the dark of the night. While they're fragile enough to crumble from a single blow, as long as a skull remains intact, they'll continue to pull themselves back together and go on fighting. Sometimes a body will pick up the wrong skull, but this doesn't seem to be a problem...`
+        description: `The remains of Bokoblins appear in the dark of the night. While they're fragile enough to crumble from a single blow, as long as a skull remains intact, they'll continue to pull themselves back together and go on fighting. Sometimes a body will pick up the wrong skull, but this doesn't seem to be a problem...`,
+        image: `https://i.imgur.com/9cR2uw4.jpg`
     },
     {
         compendiumId: 107,
@@ -93,7 +95,8 @@ const monstersData = [
             'Sapphire',
             'Diamond'
         ],
-        description: `You would be foolish to call these Silver Bokoblins a mere nuisance. They have been influenced by Ganon's fiendish magic, so they are stronger than even the Black Bokoblins. They are called "silver" not only cause of their coloring, but also to donate their rarity. The purple markings help them stand out even more.`
+        description: `You would be foolish to call these Silver Bokoblins a mere nuisance. They have been influenced by Ganon's fiendish magic, so they are stronger than even the Black Bokoblins. They are called "silver" not only cause of their coloring, but also to donate their rarity. The purple markings help them stand out even more.`,
+        image: `https://i.imgur.com/6NnIwpG.jpg"`
     },
 ]
 
